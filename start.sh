53264cf12f62301dfcdadbb0bfdd7da67de62d77
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-08-03 16:46:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone 
-https://github.com/Shashankls/DQ-the-file-donor.git /DQTheFileDonorBot
+https://github.com/Shashankls/DQ-the-file-donor.git /
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
